72d0909d0d11d4d612362b0b234652df0aabc372
Travis Fischer <fisch0920@gmail.com>
2021-08-20 19:46:06 -0400

    🤽

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as dat from 'dat.gui'
 
-// import audioUrl from '../audio/Wizard-DreamOn.mp3'
-// import audioUrl from '../audio/ChillyGonzales-SampleThis.mp3'
-import audioUrl from '../audio/voice.m4a'
-// import audioUrl from '../audio/EverythingPersists-AndThenYouSeeIt.mp3'
-// import audioUrl from '../audio/FortMinor-WheredYouGo.mp3'
+import audioUrl0 from '../audio/voice.m4a'
+import audioUrl1 from '../audio/Wizard-DreamOn.mp3'
+import audioUrl2 from '../audio/ChillyGonzales-SampleThis.mp3'
+import audioUrl3 from '../audio/EverythingPersists-AndThenYouSeeIt.mp3'
+import audioUrl4 from '../audio/FortMinor-WheredYouGo.mp3'
 
 // import { CanvasAudioVisualization as AudioViz } from './CanvasAudioVisualization'
 // import { HybridAudioVisualization as AudioViz } from './HybridAudioVisualization'
@@ -15,13 +15,22 @@ import {
   MeydaAudioFeature
 } from './BloomFilterAudioVisualization'
 
-let vis: AudioViz
+const audioTracks: { [key: string]: string } = {
+  'li jin': audioUrl0,
+  'dream on': audioUrl1,
+  'sample this': audioUrl2,
+  'and then you see it': audioUrl3,
+  "where'd you go": audioUrl4
+}
+
+let vis: AudioViz | null = null
 
 const params = {
-  offscreenScale: 2.0,
+  audioTrack: 'li jin',
   featureExtractor: 'loudness' as MeydaAudioFeature,
   drawStyle: 'curves' as DrawStyle,
   drawShape: 'triangle' as DrawShape,
+  offscreenScale: 2.0,
   fftSize: 256,
   numberOfBarkBands: 32,
   smoothingFactor: 0.7,
@@ -31,10 +40,10 @@ const params = {
 
 const gui = new dat.GUI({})
 gui
-  .add(params, 'offscreenScale', 1.0, 4.0)
-  .step(1.0)
-  .name('offscreen')
-  .onFinishChange(restart)
+  .add(params, 'audioTrack')
+  .options(Object.keys(audioTracks))
+  .name('audio track')
+  .onFinishChange(reset)
 gui
   .add(params, 'featureExtractor')
   .options([
@@ -46,52 +55,67 @@ gui
     'powerSpectrum'
   ])
   .name('feature')
-  .onFinishChange(restart)
+  .onFinishChange(reset)
 gui
   .add(params, 'drawShape')
   .options(['triangle', 'basic'])
   .name('shape')
   .onChange((value) => {
-    vis.drawShape = value
+    if (vis) {
+      vis.drawShape = value
+    }
   })
 gui
   .add(params, 'drawStyle')
   .options(['curves', 'lines', 'bars'])
   .name('style')
   .onChange((value) => {
-    vis.drawStyle = value
+    if (vis) {
+      vis.drawStyle = value
+    }
   })
+gui
+  .add(params, 'offscreenScale', 1.0, 4.0)
+  .step(1.0)
+  .name('offscreen')
+  .onFinishChange(reset)
 const fftC = gui.add(params, 'fftSize', 32, 4096).onFinishChange((value) => {
   if (!isPowerOf2(value)) {
     fftC.setValue(nextPowerOf2(value))
   }
-  restart()
+  reset()
 })
 gui
   .add(params, 'numberOfBarkBands', 4, 128)
   .step(1.0)
   .name('loudness bins')
-  .onFinishChange(restart)
+  .onFinishChange(reset)
 gui
   .add(params, 'smoothingFactor', 0.0, 1.0)
   .step(0.000001)
   .name('smoothing')
   .onChange((value) => {
-    vis.smoothingFactor = value
+    if (vis) {
+      vis.smoothingFactor = value
+    }
   })
 gui
   .add(params, 'accentuationFactor', 1.0, 16.0)
   .step(0.1)
   .name('accentuation')
   .onChange((value) => {
-    vis.accentuationFactor = value
+    if (vis) {
+      vis.accentuationFactor = value
+    }
   })
 gui
   .add(params, 'visualScalingFactor', 0.00001, 3.0)
   .step(0.1)
   .name('visual scaling')
   .onChange((value) => {
-    vis.visualScalingFactor = value
+    if (vis) {
+      vis.visualScalingFactor = value
+    }
   })
 
 function isPowerOf2(value: number): boolean {
@@ -102,11 +126,18 @@ function nextPowerOf2(value: number): number {
   return Math.pow(2, Math.ceil(Math.log2(value)))
 }
 
-const play = document.getElementById('play')
+function reset() {
+  if (!vis) {
+    return
+  }
+
+  restart()
+}
 
 function restart() {
   if (vis) {
     vis.stop()
+    vis = null
   }
 
   const canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -115,12 +146,13 @@ function restart() {
 
   vis = new AudioViz({
     canvas,
-    mediaUrl: audioUrl,
     autoplay: false,
-    ...params
+    ...params,
+    mediaUrl: audioTracks[params.audioTrack]
   })
 
   vis.start()
 }
 
+const play = document.getElementById('play')
 play?.addEventListener('click', restart)
